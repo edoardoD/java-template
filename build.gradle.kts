cdb9e65469ec23f8326000627bb5114f4f3238d6
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-06-26 19:04:57 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    // Apply the java plugin to add support for Java
+    java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.danilopianini.gradle-java-qa") version "0.43.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val javaFXModules = listOf(
+    "base",
+    "controls",
+    "fxml",
+    "swing",
+    "graphics"
+)
+
+val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
+
+dependencies {
+    // Suppressions for SpotBugs
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
+
+    val jUnitVersion = "5.9.2"
+    // JUnit API and testing engine
+    // https://mavenlibs.com/maven/dependency/org.junit.jupiter/junit-jupiter-api
+    implementation ("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+}
+
+tasks.withType<Test> {
+    // Enables JUnit 5 Jupiter module
+    useJUnitPlatform()
+}
+
+application {
+    // Define the main class for the application
+    mainClass.set("myApp")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "myApp"
+    }
+}
+
+task("create-dirs") {
+  doLast {
+    sourceSets.forEach { sourceSet ->
+      sourceSet.java.srcDirs.forEach { it.mkdirs() }
+      sourceSet.resources.srcDirs.forEach { it.mkdirs() }
+    }
+  }
+}
